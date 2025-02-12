7198625b2e01df99f55d6f5a4540c36a1c2d64f4
Gopish Mahajan <91310150+gopish12@users.noreply.github.com>
2025-02-12 14:11:22 +0530

    Delete Spring-Boot-CRUD/target directory

--- Spring-Boot-CRUD/target/temp/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Run the Java application from the JAR file
-java -jar /app/Spring-Boot-CRUD-0.0.1-SNAPSHOT.jar --spring.config.location=file:/opt/application.properties --server.port=8081
-
-
